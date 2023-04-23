0f2ca872eafe87fa4d938b6df978f13461114e82
quietThinker2 <127311105+quietThinker2@users.noreply.github.com>
2023-04-23 00:44:12 -0600

    Implemented Hard Computer Mode
    Intro has Hard Computer mode option available. Hard Computer mode can block and go for the win.

--- Main.kt
+++ Main.kt
@@ -4,6 +4,18 @@ import kotlin.system.exitProcess
 var boardData = mutableListOf("A","B","C","D","E","F","G","H","I")
 var running = true
 
+// Define the constant list of winning paths
+val WINNING_PATHS = listOf(
+    listOf(0, 1, 2), // Top row
+    listOf(3, 4, 5), // Middle row
+    listOf(6, 7, 8), // Bottom row
+    listOf(0, 3, 6), // Left column
+    listOf(1, 4, 7), // Middle column
+    listOf(2, 5, 8), // Right column
+    listOf(0, 4, 8), // Top-left to bottom-right diagonal
+    listOf(2, 4, 6) // Top-right to bottom-left diagonal
+)
+
 // This function is to start up the intro function of the Tic Tac Toe Program
 fun main() {
     playIntro()
@@ -12,9 +24,10 @@ fun main() {
 // This function is to display game modes to the user and have them pick a mode
 fun playIntro(){
     println("Tic Tac Toe")
-    println("1 Against Easy Computer")
-    println("2 Against Player")
-    println("3 Exit Program")
+    println("1 Player VS Easy Computer")
+    println("2 Player VS Hard Computer")
+    println("3 Player VS Player")
+    println("4 Exit Program")
     println("Press a Number: ")
 
 
@@ -29,26 +42,36 @@ fun playIntro(){
 
         // The Player VS Easy Computer Mode
         "1" -> while (running){
-            showBoard()
+            displayBoard()
             playerPrompt("X")
             checkWinner()
             if (running){
-                computerPrompt()
+                computerPromptEasy()
                 checkWinner()
             }
         }
-        // The Player BS Player Mode
+        // The Player VS Hard Computer Mode
         "2" -> while (running){
-            showBoard()
+            displayBoard()
             playerPrompt("X")
             checkWinner()
             if (running){
-                showBoard()
+                computerPromptHard()
+                checkWinner()
+            }
+        }
+        // The Player VS Player Mode
+        "3" -> while (running){
+            displayBoard()
+            playerPrompt("X")
+            checkWinner()
+            if (running){
+                displayBoard()
                 playerPrompt("O")
                 checkWinner()
             }
         }
-        "3" -> exitProcess(0)
+        "4" -> exitProcess(0)
         // For invalid input restart the playIntro function
         else -> playIntro()
     }
@@ -56,7 +79,7 @@ fun playIntro(){
 }
 
 // This function displays the board with any changes made
-fun showBoard(){
+fun displayBoard(){
     println()
     println("  "+boardData[0]+" |  "+boardData[1]+" |  "+boardData[2]+" ")
     println("--------------")
@@ -99,23 +122,75 @@ fun playerPrompt(playerSymbol : String){
         playerPrompt(playerSymbol)
     }
 }
-// Easy Computer function to randonly place a O on a valid spot
-fun computerPrompt(){
+// Easy Computer function to randomly place a O on a valid square
+fun computerPromptEasy(){
     val randomNum = Random.nextInt(0,8)
     if (boardData[randomNum] == "X" || boardData[randomNum] == "O") {
-        computerPrompt()
+        computerPromptEasy()
     } else {
         boardData[randomNum] = "O"
     }
+}
 
+fun computerPromptHard() {
+    // Check if the Computer can win
+    // Check if the Computer can block the player
+    // Take the Center
+    //
 
+    // Check if the computer can Win
+    for (path in WINNING_PATHS) {
+        val count = path.count { boardData[it] == "O" }
+        if (count == 2) {
+            // Find the missing 'X'
+            val missingIndex = getMissingIndex(path)
+            if (missingIndex != null) {
+                // Make the winning move
+                boardData[missingIndex] = "O"
+                return
+            }
+        }
+    }
+    // Check if the computer can Block
+    for (path in WINNING_PATHS) {
+        val count = path.count { boardData[it] == "X" }
+        if (count == 2) {
+            // Find the missing 'X'
+            val missingIndex = getMissingIndex(path)
+            if (missingIndex != null) {
+                // Make the winning move
+                boardData[missingIndex] = "O"
+                return
+            }
+        }
+    }
+
+    // Have the Computer take the center if possible
+    if (boardData[4] != "X" && boardData[4] != "O"){
+        boardData[4] = "O"
+        return
+    }
+
+    // If the computer can't win/block/take center, make a random move
+    val emptyIndexes = boardData.indices.filter { boardData[it] != "X" && boardData[it] != "O" }
+    val randomIndex = emptyIndexes.random()
+    boardData[randomIndex] = "O"
+}
+
+fun getMissingIndex(path: List<Int>): Int? {
+    for (index in path) {
+        if (boardData[index] != "X" && boardData[index] != "O") {
+            return index
+        }
+    }
+    return null
 }
 
 fun checkWinner(){
     // check for a horizontal win
     for (i in 0..6 step 3) {
         if (boardData[i] == boardData[i+1] && boardData[i+1] == boardData[i+2] && boardData[i].isNotBlank()) {
-            showBoard()
+            displayBoard()
             println("${boardData[i]} wins horizontal!")
 
             // Reset board and start intro
@@ -129,7 +204,7 @@ fun checkWinner(){
     // check for a vertical win
     for (i in 0..2) {
         if (boardData[i] == boardData[i+3] && boardData[i+3] == boardData[i+6] && boardData[i].isNotBlank()) {
-            showBoard()
+            displayBoard()
             println("${boardData[i]} wins vertical!")
 
             // Reset board and start intro
@@ -142,7 +217,7 @@ fun checkWinner(){
 
     // check for a diagonal win
     if (boardData[0] == boardData[4] && boardData[4] == boardData[8] && boardData[0].isNotBlank()) {
-        showBoard()
+        displayBoard()
         println("${boardData[0]} wins diagonal!")
 
         // Reset board and start intro
@@ -154,7 +229,7 @@ fun checkWinner(){
 
     // check for other diagonal win
     if (boardData[2] == boardData[4] && boardData[4] == boardData[6] && boardData[2].isNotBlank()) {
-        showBoard()
+        displayBoard()
         println("${boardData[2]} wins diagonal!")
 
         // Reset board and start intro
@@ -165,9 +240,9 @@ fun checkWinner(){
     }
 
     // check if there is a stalemate
-    var containsOnlyXandO = boardData.all { it == "X" || it == "O" }
+    val containsOnlyXandO = boardData.all { it == "X" || it == "O" }
     if (containsOnlyXandO) {
-        showBoard()
+        displayBoard()
         println("Stalemate no one wins!")
 
         // Reset board and start intro
